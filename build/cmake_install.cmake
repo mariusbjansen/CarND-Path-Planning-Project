dc7543deb346ec10eadb9dedaa9838fd856ed06e
Marius Jansen <mariusb.jansen@googlemail.com>
2018-05-05 21:47:23 +0200

    introduced hints from video tutorial, attention buggy not working yet, clanged

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/bruno/udacity/carnd3/CarND-Path-Planning-Project
+# Install script for directory: /home/bruno/udacity/selfdrivingcar/Term3/CarND-Path-Planning-Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/bruno/udacity/carnd3/CarND-Path-Planning-Project/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/bruno/udacity/selfdrivingcar/Term3/CarND-Path-Planning-Project/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
